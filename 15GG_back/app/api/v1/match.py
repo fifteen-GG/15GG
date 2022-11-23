67326dd2954ac201222429d8fd803b56fad4b247
jeongbinboo <jrkasey3461@naver.com>
2022-11-24 00:20:52 +0900

    feat: [OD15-125] Add updating match status & Add returning analyzing match list

---
+++ 15GG_back/app/api/v1/match.py
@@ -0,0 +1,25 @@
+from datetime import datetime, timedelta
+from http.client import HTTPException
+from app.api.v1.riot_api import get_match_data, get_match_data_list
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app import schemas, crud
+from app.api.deps import get_db
+
+import httpx
+
+router = APIRouter()
+
+
+@router.get('/analyzing')
+def get_analyzing_match(*, db: Session = Depends(get_db)):
+    analyzing_match_list = crud.match.get_analyzing_match(db)
+
+    return analyzing_match_list
+
+
+@router.post('/update/status')
+def update_match_status(*, db: Session = Depends(get_db), match_id: str, status: int):
+    crud.match.update_match_status(db, match_id, status)

5f3949a1296de435be8ceb1fd695a81e73672087
SungChul Hong <tjd000222@naver.com>
2022-11-29 17:31:56 +0900

    fix: for CORS error

--- 15GG_front/src/WebClient.ts
+++ 15GG_front/src/WebClient.ts
@@ -4,6 +4,7 @@ const webClient: AxiosInstance = axios.create({
   baseURL: process.env.REACT_APP_GG_API_ROOT,
   headers: {
     'Access-Control-Allow-Private-Network': true,
+    'Access-Control-Allow-Origin': '*',
   },
 });
 
